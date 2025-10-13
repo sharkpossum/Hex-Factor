f73d0e413bc5bd3c44bbbdf1a2653a84b932ab85
sharkpossum <dreamingshark@proton.me>
2025-10-13 20:25:33 +0200

    Added head bobbing, moved player stats like speed into resource file instead

---
+++ scenes/player/Player_Stats.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+class_name Player_Stats
+
+@export var SPEED = 5.0
+@export var WALK_SPEED = 5.0
+@export var SPRINT_SPEED = 8.0
+@export var JUMP_VELOCITY = 4.5
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,23 +1,34 @@
 extends CharacterBody3D
 
-@export var config: Player_Config;
+@export var config: Player_Config
+@export var stats: Player_Stats
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+# Head bobbing
+@export var BOB_FREQ : float = 2.0
+@export var BOB_AMP : float = 0.06
+var bobTime: float = 0.0
 
 @onready var head = %Head
 @onready var playerCam = %PlayerCamera
 
 func _ready():
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED;
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	pass
 	
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		head.rotate_y(event.relative.x * -1 * config.MOUSE_SENSITIVITY)
 		playerCam.rotate_x(event.relative.y * -1 * config.MOUSE_SENSITIVITY)
-		playerCam.rotation.x = clamp(playerCam.rotation.x, deg_to_rad(-40), deg_to_rad(60))
+		playerCam.rotation.x = clamp(playerCam.rotation.x, deg_to_rad(-60), deg_to_rad(60))
 		pass
+	pass
+	
+func _head_bob(delta: float, velocity: Vector3):
+	bobTime += delta * velocity.length() * float(is_on_floor())
+	var bobPos = Vector3.ZERO;
+	bobPos.y = sin(bobTime * BOB_FREQ) * BOB_AMP
+	bobPos.x = sin(bobTime * BOB_FREQ / 2) * BOB_AMP
+	head.transform.origin = bobPos
 	pass
 
 func _physics_process(delta: float) -> void:
@@ -27,7 +38,7 @@ func _physics_process(delta: float) -> void:
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		velocity.y = stats.JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -36,10 +47,11 @@ func _physics_process(delta: float) -> void:
 	# Set direction basis to head direction.
 	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity.x = direction.x * stats.SPEED
+		velocity.z = direction.z * stats.SPEED
 	else:
 		velocity.x = 0
 		velocity.z = 0
 
 	move_and_slide()
+	_head_bob(delta, velocity)
